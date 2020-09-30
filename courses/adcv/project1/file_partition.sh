dd09d7cea869b7297b66060b2884bc681ccde84f
Byron-Edwards <byron_edwards@outlook.com>
2020-09-30 17:22:04 +0800

    add file partition

---
+++ courses/adcv/project1/file_partition.sh
@@ -0,0 +1,3 @@
+sed -n 3,162772p list_attr_celeba.txt > train_40_att_list.txt
+sed -n 162773,182639p list_attr_celeba.txt > val_40_att_list.txt
+sed -n 182640,202601p list_attr_celeba.txt > test_40_att_list.txt
